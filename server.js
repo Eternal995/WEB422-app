5181022e575cdec8f7d58b4a6ac9c43b16b2ccaa
Yongda Long <ylong9@myseneca.ca>
2023-01-15 19:38:56 -0500

    Update Assignment 1

--- server.js
+++ server.js
@@ -27,6 +27,17 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Initialize
+db.initialize(process.env.MONGODB_CONN_STRING)
+  .then(() => {
+    app.listen(HTTP_PORT, function () {
+      console.log(`server listening on: ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // Routes
 
 // Home
@@ -99,13 +110,3 @@ app.use((req, res) => {
   res.status(404).send("404 Page Not Found!");
 });
 
-// Initialize
-db.initialize(process.env.MONGODB_CONN_STRING)
-  .then(() => {
-    app.listen(HTTP_PORT, function () {
-      console.log(`server listening on: ${HTTP_PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
