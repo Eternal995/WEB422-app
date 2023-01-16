9ca2b7d7e35960b05092621d5c92531f2e1a20e3
Yongda Long <ylong9@myseneca.ca>
2023-01-15 19:18:18 -0500

    Update Assignment 1

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ *  WEB422 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Yongda Long Student ID: 172800211 Date: Jan 15, 2023
+ *  Cyclic Link:
+ *
+ ********************************************************************************/
+
 // Setup
 const express = require("express");
 const path = require("path");
@@ -13,7 +24,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
+
+// Home
 app.get("/", (req, res) => {
+  res.json({ message: "API Listening" });
+});
+
+app.get("/home", (req, res) => {
   res.json({ message: "API Listening" });
 });
 
